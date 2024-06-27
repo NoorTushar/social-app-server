c1ba22ba55536f15651625b8fafdb3da169bdced
NoorTushar <tushar.noor.khan@gmail.com>
2024-06-27 21:41:58 +0600

    user registration successful

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
@@ -14,7 +16,6 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j7c4zww.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,6 +31,60 @@ async function run() {
    try {
       const usersCollection = client.db("social-app").collection("users");
       const postsCollection = client.db("social-app").collection("users");
+
+      /********** JWT Related APIs ************/
+
+      // Create a token against a user email
+      app.post("/jwt", async (req, res) => {
+         const user = req.body;
+         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "7d",
+         });
+         res.send({ token });
+      });
+
+      // middlewares:
+      const verifyToken = (req, res, next) => {
+         console.log(req.headers);
+         console.log(
+            "from middleware verify token: ",
+            req.headers.authorization
+         );
+
+         if (!req.headers.authorization) {
+            return res.status(401).send({ message: "Unauthorized Access" });
+         }
+
+         const token = req.headers.authorization.split(" ")[1];
+
+         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+               return res.status(401).send({ message: "Unauthorized Access" });
+            }
+
+            req.decoded = decoded;
+
+            next();
+         });
+      };
+
+      /********** Users Related APIs ************/
+
+      // enter a employee if not already added to database
+      app.post("/users", async (req, res) => {
+         const data = req.body;
+         console.log("registered/ google sign in: ", data);
+         const email = data.email;
+         const query = { email: email };
+
+         const isExist = await usersCollection.findOne(query);
+         console.log("is Exist in db already: ", isExist);
+         if (isExist) {
+            return res.send(isExist);
+         }
+         const result = await usersCollection.insertOne(data);
+         res.send(result);
+      });
 
       app.get("/users", async (req, res) => {
          const result = await usersCollection.find().toArray();
